a3843e799823b2bbb6757608bfb2297fcc598f68
stalary <452024236@qq.com>
2018-01-04 16:09:07 +0800

    first commit

---
+++ src/main/java/com/stalary/book/data/ResponseMessage.java
@@ -0,0 +1,47 @@
+/**
+ * @(#)ResponseMessage.java, 2018-01-04.
+ * <p>
+ * Copyright 2018 Youdao, Inc. All rights reserved.
+ * YOUDAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.stalary.book.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * ResponseMessage
+ *
+ * @author lirongqian
+ * @since 2018/01/04
+ */
+@Data
+@AllArgsConstructor
+public class ResponseMessage {
+
+    private int code;
+
+    private String msg;
+
+    private String description;
+
+    private Object data;
+
+    public ResponseMessage(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public static ResponseMessage enumError(ResultEnum resultEnum) {
+        return new ResponseMessage(resultEnum.getCode(), resultEnum.getMsg());
+    }
+
+    public static ResponseMessage successMessage(Object data) {
+        return new ResponseMessage(0, "success", null, data);
+    }
+
+    public static ResponseMessage failedMessage(String message) {
+        return new ResponseMessage(1, "failed", message, null);
+    }
+
+}
