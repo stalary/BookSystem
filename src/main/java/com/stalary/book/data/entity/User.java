3800b7342bd55643f4e1099bbb31f14e0472d457
Hawk <wsghawk97@163.com>
2018-02-17 11:15:49 +0800

    ADD:图书实体类

--- src/main/java/com/stalary/book/data/entity/User.java
+++ src/main/java/com/stalary/book/data/entity/User.java
@@ -10,10 +10,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-
 /**
  * User
  *
